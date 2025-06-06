0f2332ec4280accac0feefb7e14a9d8a2a9f1173
Tania <tl303132@gmail.com>
2025-06-06 13:23:24 +0200

    added simple tests

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,6 +1,10 @@
 package tests;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -8,17 +12,31 @@ public class TextBoxTests extends TestBase{
 
     @Test
     void fillFormTest() {
+        Faker faker = new Faker(new Locale("de-DE"));
+
+/*        String
+                userName = "Masha",
+                userEmail = "masha@gmail.com",
+                currentAddress = "Some street 1",
+                permanentAddress = "Some street 2";*/
+
+        String
+                userName = faker.name().fullName(),
+                userEmail = faker.internet().emailAddress(),
+                currentAddress = faker.address().fullAddress(),
+                permanentAddress = faker.address().fullAddress();
+
         open("/text-box");
-        $("#userName").setValue("Masha");
-        $("#userEmail").setValue("masha@gmail.com");
-        $("#currentAddress").setValue("Some street 1");
-        $("#permanentAddress").setValue("Some street 2");
+        $("#userName").setValue(userName);
+        $("#userEmail").setValue(userEmail);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
         executeJavaScript("window.scrollTo(0, document.body.scrollHeight);");
         $("#submit").click();
 
-        $("#output #name").shouldHave(text("Masha"));
-        $("#output #email").shouldHave(text("masha@gmail.com"));
-        $("#output #currentAddress").shouldHave(text("Some street 1"));
-        $("#output #permanentAddress").shouldHave(text("Some street 2"));
+        $("#output #name").shouldHave(text(userName));
+        $("#output #email").shouldHave(text(userEmail));
+        $("#output #currentAddress").shouldHave(text(currentAddress));
+        $("#output #permanentAddress").shouldHave(text(permanentAddress));
     }
 }
